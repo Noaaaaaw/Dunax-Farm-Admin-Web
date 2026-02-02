5273e43c800184daf3c2f15a5fe5ff6aa78e3dd1
Anooaa <leegaeun250@gmail.com>
2026-02-02 15:47:45 +0700

    Fix with SSL 2

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,6 @@ import pkg from 'pg';
 import 'dotenv/config'; 
 const { Pool } = pkg;
 
-// Baris console.log sudah gue hapus biar terminal bersih
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
