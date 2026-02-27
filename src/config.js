eb0e2a0fede53110d81cbb7539bfd606cd7c5c3e
Anooaa <leegaeun250@gmail.com>
2026-02-27 15:28:40 +0700

    Update 71

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const CONFIG = {
-  BASE_URL: 'http://103.171.85.57:5000',
+  BASE_URL: 'http://dunax-api.duckdns.org:5000',
   SUPABASE_URL: 'https://cdhpijazjuuvujxlhigq.supabase.co',
   SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkaHBpamF6anV1dnVqeGxoaWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3ODQzMTgsImV4cCI6MjA4NTM2MDMxOH0.iSldvMGaUauu_SoxnbuzF83YvP8ikDaePi-_g15Q33g'
 };
